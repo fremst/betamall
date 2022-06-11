0fbee44800e51a8e512cbac4dd3cd355680178bf
Jiyeon Kim <cleo0718@gmail.com>
2022-06-12 06:10:16 +0900

    Revert "BM-23 회원가입 최소기능 구현(경로 추후수정)"
    This reverts commit 8afa9846db3a587a984a0f151c37f78ef372b270.

--- src/main/java/com/betamall/controller/HomeController.java
+++ src/main/java/com/betamall/controller/HomeController.java
@@ -2,20 +2,21 @@ package com.betamall.controller;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/main")
+@WebServlet("/home")
 @SuppressWarnings("serial")
 public class HomeController extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		ServletContext application = getServletContext();
-		application.setAttribute("cp", req.getContextPath());
-		req.getRequestDispatcher("views/home/main.jsp").forward(req, resp); //로그인뒤 경로 수정해야함 
+		
+		// main에 보여질 페이지를 requestScope에 담기
+		req.setAttribute("mainPage", "/views/common/main.jsp");
+		// layout.jsp (header + main + footer) 호출
+		req.getRequestDispatcher("/views/common/layout.jsp").forward(req, resp);
 	}
 }
