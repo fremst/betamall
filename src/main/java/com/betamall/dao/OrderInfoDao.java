a62ea8fae0307802fb107dc8fcaafceed693a222
Jiyeon Kim <cleo0718@gmail.com>
2022-06-23 02:36:00 +0900

    BM-12 회원 마이페이지 구매목록
    구매목록 구현
    배송중 -> 구매확정버튼 활성화
    구매확정버튼 클릭시 후기 작성 페이지로 이동
    구매확정 => DB상태 바뀜
    css완

---
+++ src/main/java/com/betamall/dao/OrderInfoDao.java
@@ -0,0 +1,189 @@
+package com.betamall.dao;
+
+import com.betamall.dto.OrderInfoDto;
+import com.betamall.util.JdbcUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OrderInfoDao {
+
+    private static OrderInfoDao instance = new OrderInfoDao();
+
+    private OrderInfoDao() {
+    }
+
+    public static OrderInfoDao getInstance() {
+        return instance;
+    }
+
+    public ArrayList<OrderInfoDto> selectByBrNQuery(int brNo, String query) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        if (query == null) {
+            query = "";
+        }
+        try {
+            con = JdbcUtil.getCon();
+            String sql = "SELECT ORDNO, ITEMNO, ITEMNAME, ORDCNT, PRICE, ORDDATE, ORDSTA, MBRID, ORDNAME, ORDTEL, ORDARRD "
+                    + "FROM (((\"ORDER\" INNER JOIN ORDITEM USING(ORDNO)) INNER JOIN ITEM USING(ITEMNO))) INNER JOIN MEMBER USING (MBRNO) "
+                    + "WHERE BRNO = ? ";
+            sql += query;
+            sql += "ORDER BY ORDNO DESC";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, brNo);
+            rs = pstmt.executeQuery();
+            ArrayList<OrderInfoDto> ordInfoDtos = new ArrayList<OrderInfoDto>();
+            while (rs.next()) {
+                ordInfoDtos.add(
+                        new OrderInfoDto(
+                                rs.getInt("ORDNO"),
+                                rs.getInt("ITEMNO"),
+                                rs.getString("ITEMNAME"),
+                                rs.getInt("ORDCNT"),
+                                rs.getInt("PRICE"),
+                                rs.getDate("ORDDATE"),
+                                rs.getString("ORDSTA"),
+                                rs.getString("MBRID"),
+                                rs.getString("ORDNAME"),
+                                rs.getString("ORDTEL"),
+                                rs.getString("ORDARRD")
+                        )
+                );
+            }
+            return ordInfoDtos;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            JdbcUtil.close(con, pstmt, rs);
+        }
+    }
+
+    public OrderInfoDto selectByMbrNo(int mbrNo) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = JdbcUtil.getCon();
+            String sql = "SELECT ORDNO, ITEMNO, ITEMNAME, ORDCNT, PRICE, ORDDATE, ORDSTA, MBRID, ORDNAME, ORDTEL, ORDARRD "
+                    + "FROM (((\"ORDER\" INNER JOIN ORDITEM USING(ORDNO)) INNER JOIN ITEM USING(ITEMNO))) INNER JOIN MEMBER USING (MBRNO) "
+                    + "WHERE MBRNO = ? ";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, mbrNo);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new OrderInfoDto(
+                        rs.getInt("ORDNO"),
+                        rs.getInt("ITEMNO"),
+                        rs.getString("ITEMNAME"),
+                        rs.getInt("ORDCNT"),
+                        rs.getInt("PRICE"),
+                        rs.getDate("ORDDATE"),
+                        rs.getString("ORDSTA"),
+                        rs.getString("MBRID"),
+                        rs.getString("ORDNAME"),
+                        rs.getString("ORDTEL"),
+                        rs.getString("ORDARRD")
+                );
+            }
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            JdbcUtil.close(con, pstmt, rs);
+        }
+    }
+
+    public ArrayList<OrderInfoDto> selectAllByMbrNo(int mbrNo) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = JdbcUtil.getCon();
+            String sql = "SELECT ORDNO, ITEMNO, ITEMNAME, ORDCNT, PRICE, ORDDATE, ORDSTA, MBRID, ORDNAME, ORDTEL, ORDARRD , TIMG "
+                    + "FROM (((\"ORDER\" INNER JOIN ORDITEM USING(ORDNO)) INNER JOIN ITEM USING(ITEMNO))) INNER JOIN MEMBER USING (MBRNO) "
+                    + "WHERE MBRNO = ? "
+                    + "ORDER BY ORDNO DESC";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, mbrNo);
+            rs = pstmt.executeQuery();
+            ArrayList<OrderInfoDto> mbrOrderInfoDtos = new ArrayList<>();
+            while (rs.next()) {
+                mbrOrderInfoDtos.add(
+                        new OrderInfoDto(
+                                rs.getInt("ORDNO"),
+                                rs.getInt("ITEMNO"),
+                                rs.getString("ITEMNAME"),
+                                rs.getInt("ORDCNT"),
+                                rs.getInt("PRICE"),
+                                rs.getDate("ORDDATE"),
+                                rs.getString("ORDSTA"),
+                                rs.getString("MBRID"),
+                                rs.getString("ORDNAME"),
+                                rs.getString("ORDTEL"),
+                                rs.getString("ORDARRD"),
+                                rs.getString("TIMG")
+                        )
+                );
+            }
+            return mbrOrderInfoDtos;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            JdbcUtil.close(con, pstmt, rs);
+        }
+    }
+
+    public ArrayList<OrderInfoDto> selectByMbrNQuery(int mbrNo, String query) {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        if (query == null) {
+            query = "";
+        }
+        try {
+            con = JdbcUtil.getCon();
+            String sql = "SELECT ORDNO, ITEMNO, ITEMNAME, ORDCNT, PRICE, ORDDATE, ORDSTA, MBRID, ORDNAME, ORDTEL, ORDARRD , TIMG "
+                    + "FROM (((\"ORDER\" INNER JOIN ORDITEM USING(ORDNO)) INNER JOIN ITEM USING(ITEMNO))) INNER JOIN MEMBER USING (MBRNO) "
+                    + "WHERE MBRNO = ? ";
+            sql += query;
+            sql += "ORDER BY ORDNO DESC";
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, mbrNo);
+            rs = pstmt.executeQuery();
+            ArrayList<OrderInfoDto> ordInfoDtos = new ArrayList<OrderInfoDto>();
+            while (rs.next()) {
+                ordInfoDtos.add(
+                        new OrderInfoDto(
+                                rs.getInt("ORDNO"),
+                                rs.getInt("ITEMNO"),
+                                rs.getString("ITEMNAME"),
+                                rs.getInt("ORDCNT"),
+                                rs.getInt("PRICE"),
+                                rs.getDate("ORDDATE"),
+                                rs.getString("ORDSTA"),
+                                rs.getString("MBRID"),
+                                rs.getString("ORDNAME"),
+                                rs.getString("ORDTEL"),
+                                rs.getString("ORDARRD"),
+                                rs.getString("TIMG")
+                        )
+                );
+            }
+            return ordInfoDtos;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            JdbcUtil.close(con, pstmt, rs);
+        }
+    }
+
+}
---
+++ src/main/java/com/betamall/dto/OrderInfoDto.java
@@ -0,0 +1,168 @@
+package com.betamall.dto;
+
+import java.sql.Date;
+
+public class OrderInfoDto {
+
+    private int ordNo;
+    private int itemNo;
+    private String itemName;
+    private int ordCnt;
+    private int price;
+    private Date ordDate;
+    private String ordSta;
+    private String mbrId;
+    private String ordName;
+    private String ordTel;
+    private String ordAddr;
+    private String tImg;
+
+    public OrderInfoDto() {
+    }
+
+
+    public OrderInfoDto(int ordNo, int itemNo, String itemName, int ordCnt, int price, Date ordDate, String ordSta,
+                        String mbrId, String ordName, String ordTel, String ordAddr) {
+
+        this.ordNo = ordNo;
+        this.itemNo = itemNo;
+        this.itemName = itemName;
+        this.ordCnt = ordCnt;
+        this.price = price;
+        this.ordDate = ordDate;
+        this.ordSta = ordSta;
+        this.mbrId = mbrId;
+        this.ordName = ordName;
+        this.ordTel = ordTel;
+        this.ordAddr = ordAddr;
+    }
+
+    public OrderInfoDto(int ordNo, int itemNo, String itemName, int ordCnt, int price, Date ordDate, String ordSta, String mbrId, String ordName, String ordTel, String ordAddr, String tImg) {
+        this.ordNo = ordNo;
+        this.itemNo = itemNo;
+        this.itemName = itemName;
+        this.ordCnt = ordCnt;
+        this.price = price;
+        this.ordDate = ordDate;
+        this.ordSta = ordSta;
+        this.mbrId = mbrId;
+        this.ordName = ordName;
+        this.ordTel = ordTel;
+        this.ordAddr = ordAddr;
+        this.tImg = tImg;
+    }
+
+    public int getOrdNo() {
+        return ordNo;
+    }
+
+    public void setOrdNo(int ordNo) {
+        this.ordNo = ordNo;
+    }
+
+    public int getItemNo() {
+        return itemNo;
+    }
+
+    public void setItemNo(int itemNo) {
+        this.itemNo = itemNo;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getOrdCnt() {
+        return ordCnt;
+    }
+
+    public void setOrdCnt(int ordCnt) {
+        this.ordCnt = ordCnt;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getOrdDate() {
+        return ordDate;
+    }
+
+    public void setOrdDate(Date ordDate) {
+        this.ordDate = ordDate;
+    }
+
+    public String getOrdSta() {
+        return ordSta;
+    }
+
+    public void setOrdSta(String ordSta) {
+        this.ordSta = ordSta;
+    }
+
+    public String getMbrId() {
+        return mbrId;
+    }
+
+    public void setMbrId(String mbrId) {
+        this.mbrId = mbrId;
+    }
+
+    public String getOrdName() {
+        return ordName;
+    }
+
+    public void setOrdName(String ordName) {
+        this.ordName = ordName;
+    }
+
+    public String getOrdTel() {
+        return ordTel;
+    }
+
+    public void setOrdTel(String ordTel) {
+        this.ordTel = ordTel;
+    }
+
+    public String getOrdAddr() {
+        return ordAddr;
+    }
+
+    public void setOrdAddr(String ordAddr) {
+        this.ordAddr = ordAddr;
+    }
+
+    public String gettImg() {
+        return tImg;
+    }
+
+    public void settImg(String tImg) {
+        this.tImg = tImg;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderInfoDto{" +
+                "ordNo=" + ordNo +
+                ", itemNo=" + itemNo +
+                ", itemName='" + itemName + '\'' +
+                ", ordCnt=" + ordCnt +
+                ", price=" + price +
+                ", ordDate=" + ordDate +
+                ", ordSta='" + ordSta + '\'' +
+                ", mbrId='" + mbrId + '\'' +
+                ", ordName='" + ordName + '\'' +
+                ", ordTel='" + ordTel + '\'' +
+                ", ordAddr='" + ordAddr + '\'' +
+                ", tImg='" + tImg + '\'' +
+                '}';
+    }
+}
